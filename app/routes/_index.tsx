c3f62615687c760bc5139e9c482b62899b7ea8c4
ILViTs <tsoyilya@gmail.com>
2023-12-28 20:53:46 +0300

    fix UI

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
     <header className='relative flex items-center justify-center overflow-hidden h-dvh bg-slate-700'>
       <div className='z-20 flex flex-col items-center justify-center w-full mx-8'>
         <img src='/static/images/hero.svg' alt='' />
-        <p className='-mt-4 text-xl'>
+        <p className='mt-4 text-base sm:text-lg sm:mt-4 md:text-xl lg:text-2xl xl:-mt-4'>
           From Dreams to Screens – FairyBrains Magic
         </p>
         {/* <Button size={"lg"}>Get Started</Button> */}
